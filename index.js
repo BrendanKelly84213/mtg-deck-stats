546d5d69fa2121a55bdaf48f5cdcf036112452b5
Brendan Kelly <brendan@debian.domain.org>
2021-02-20 13:13:03 -0800

    got basic mana counting to work

---
+++ index.js
@@ -0,0 +1,88 @@
+const mtg = require('mtgsdk');
+
+const testDeck = {
+	'goblin warchief': 16,
+	'festering goblin': 10,
+	'swamp': 24,
+};
+
+const handleErr = e => console.log('caught' + e.message);
+
+// Card object
+async function card(cardName) {
+	const cardObj = await mtg.card.where({ name: cardName })
+		.catch(handleErr);
+	return cardObj[0];
+}
+
+// Card colors object 
+async function cardColors(cardName) {
+	const c = await card(cardName).catch(handleErr);
+
+	const manaCost = c.manaCost;
+	const cmc = c.cmc;
+
+	const numColor = color => {
+		const regex = new RegExp('[^' + color + ']','g');
+		return manaCost ? manaCost.replace(regex, "").length : 0;
+	};
+
+	const _R=numColor('R'),
+	      _G=numColor('G'),
+	      _U=numColor('U'),
+	      _W=numColor('W'),
+	      _B=numColor('B');
+
+	const colorsArr = [_R,_G,_U,_W,_B];
+	const _C = cmc - colorsArr.reduce((acc,curr) => acc + curr);
+
+	return {
+		R: _R,  
+		G: _G,  
+		U: _U,  
+		W: _W,  
+		B: _B,  
+		C: _C
+	};
+}
+
+// Get the object for all the colors in the deck
+async function deckColors(deck) {
+	// Build an array of objects containing colors of cards	
+	const colorObjs = await Promise.all(
+		Object.keys(deck).map(async cardName => {
+			const numCards = deck[cardName];
+			// Get the colors object for the card
+			const colors = await cardColors(cardName).catch(handleErr);
+			// Adjust for amount for each color and place in an array
+			const adjColorsArr = Object.values(colors).map(value => value * numCards);
+			// The total number of colors corresponding to the card
+			return {
+				R: adjColorsArr[0],
+				U: adjColorsArr[1],
+				G: adjColorsArr[2],
+				W: adjColorsArr[3],
+				B: adjColorsArr[4],
+				C: adjColorsArr[5]
+			};
+		})
+	);
+
+	// Looking now for the reduced colors object array. Sum but for objects
+	// I cannot believe this just works. God bless JavaScript 
+	return colorObjs.reduce((acc, curr) => ({ 
+			R: acc.R + curr.R,
+			U: acc.U + curr.U,
+			G: acc.G + curr.G,
+			W: acc.W + curr.W,
+			B: acc.B + curr.B,
+			C: acc.C + curr.C
+		})
+	);
+}
+
+(async () => { 
+	 const colors = await deckColors(testDeck);
+	 console.log(colors);
+})();
+
